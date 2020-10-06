93cbf4fe4397c89cc18a73fc00eea30d984576ca
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-10-06 14:50:36 -0700

    Deleted duplicate code from test_helper file

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,11 +12,6 @@ require 'vcr'
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 VCR.configure do |config|
-  config.cassette_library_dir = "test/cassettes"
-  config.hook_into :webmock
-end
-
-VCR.configure do |config|
   config.cassette_library_dir = "test/cassettes" # folder where casettes will be located
   config.hook_into :webmock # tie into this other tool called webmock
   config.default_cassette_options = {
